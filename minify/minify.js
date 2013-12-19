0f3155b4c1173af64763671b7f76f184b3e3a1fa
petersirka <petersirka@gmail.com>
2013-12-19 13:17:03 +0100

    updated: license

--- minify/minify.js
+++ minify/minify.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 var path = require('path');
 var filename = process.argv[2];
-var license = '// Copyright 2012-2014 (c) Peter Širka <petersirka@gmail.com>\n//\n\n';
+var license = '// Copyright 2012-2014 (c) Peter Širka <petersirka@gmail.com>\n\n';
 fs.writeFileSync(filename, license + fs.readFileSync(filename, 'utf8'), 'utf8');
